89e0b06c1a2add44d2f4d93ad54c9fd75e1d10f7
Tafka-4 <shkh071216@gmail.com>
2025-10-01 12:20:54 +0900

    Feat: deploy.sh added

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "[INFO] Pulling new images"
+docker compose pull --quiet
+
+echo "[INFO] Stopping old containers"
+docker compose down --remove-orphans
+
+echo "[INFO] Removing old images"
+docker image prune -f
+
+echo "[INFO] Starting new containers"
+docker compose up --build -d --remove-orphans
+
+rm ~/CTF-Bot/.env
